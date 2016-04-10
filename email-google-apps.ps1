1cb0ea9eb5c7066f35b2deaa44c2912a298c4ec3
Derek Moyes <derek.moyes@rackspace.com>
2016-04-10 13:50:55 -0500

    changed from email to be more clear
    fromgmail@gmail.com

--- email-google-apps.ps1
+++ email-google-apps.ps1
@@ -1,15 +1,15 @@
 # Fill in these variables
 ##############################################################################
-$From = "youremail@example.com"
+$From = "fromgmail@gmail.com"
 $To = "youremail@example.com"
-$Cc = "youremail@example.com"
+$Cc = "anotheremail@example.com"
 # Remember if the attachment is too large, the email send will time out. 
 $Attachment = "C:\important_log_file.log"
 $Subject = "Test Subject"
 $Body = "Test Success!"
 $SMTPServer = "smtp.gmail.com" # works with Google Apps
 $SMTPPort = "587"
-$credentials = new-object Management.Automation.PSCredential "youremail@example.com", ("YOURPASSWORD" | ConvertTo-SecureString -AsPlainText -Force)
+$credentials = new-object Management.Automation.PSCredential "fromgmail@gmail.com", ("YOURPASSWORD" | ConvertTo-SecureString -AsPlainText -Force)
 
 # Actually do the work
 ##############################################################################
