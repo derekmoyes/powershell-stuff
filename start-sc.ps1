f3b1036bc67d703e98dd2a46b14a2476a657625d
Derek Moyes <derek.moyes@gmail.com>
2020-03-29 23:07:31 -0500

    Create start-sc.ps1

---
+++ start-sc.ps1
@@ -0,0 +1,39 @@
+# start-sc.ps1
+# Runs the requirements for Star Citizen, then starts the launcher.
+# 20200329 21:48 Derek Moyes <derek.moyes at gmail dot com>
+
+clear
+
+# Start JoyToKey
+# https://joytokey.net/en/
+"=== Starting JoyToKey ==="
+Start-Process "C:\Games\JoyToKey\JoyToKey.exe"
+
+# Start GameGlass 
+# https://gameglass.gg/ 
+"=== Starting GameGlass ==="
+Start-Process -FilePath "C:\Program Files\GameGlass\launcher.exe" -WorkingDirectory "C:\Program Files\GameGlass" -Verb RunAs
+
+# Check free memory
+$freemem = Get-WmiObject -Class Win32_OperatingSystem
+While (([math]::round(($freemem.FreePhysicalMemory / 1024 / 1024), 2)) -lt 10)
+    {
+	""
+	""
+	"=== Free Memory Under 10GB, currently {0} GB ===" -f ([math]::round(($freemem.FreePhysicalMemory / 1024 / 1024), 2))
+	""
+	""
+	Read-Host -Prompt "Free up some RAM, then press Enter to continue"
+	$freemem = Get-WmiObject -Class Win32_OperatingSystem
+    }
+"=== Free Memory {0} GB ===" -f ([math]::round(($freemem.FreePhysicalMemory / 1024 / 1024), 2))
+
+# Wait a number of seconds for GameGlass to start.
+$sleepseconds = 10
+"=== Waiting $sleepseconds for GameGlass to start ==="
+Start-Sleep -Second $sleepseconds
+
+# Start Star Citizen Launcher
+# https://robertsspaceindustries.com/
+"=== Starting Star Citizen Launcher ==="
+Start-Process -FilePath "C:\Games\RSI Launcher\RSI Launcher.exe" -WorkingDirectory "C:\Games\RSI Launcher"
